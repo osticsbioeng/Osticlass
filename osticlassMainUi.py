5d2225b3f1fe284e8f32477e906b32b48e734ba1
Kaname21Miura <miukana21@gmail.com>
2019-05-28 14:21:54 +0900

    MainWindow.pyを書き換えました
    .uiから.pyへの書き換えは、
    pyuic5 -o hoge.py hoge.ui
    で可能です。

--- osticlassMainUi.py
+++ osticlassMainUi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'osticlassMain.ui'
+# Form implementation generated from reading ui file 'osticlassMainUi.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
@@ -11,47 +11,48 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(400, 300)
-        self.verticalLayoutWidget = QtWidgets.QWidget(Form)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 50, 377, 181))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        Form.resize(308, 245)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.pushButton = QtWidgets.QPushButton(Form)
         self.pushButton.setIconSize(QtCore.QSize(16, 20))
         self.pushButton.setObjectName("pushButton")
-        self.verticalLayout.addWidget(self.pushButton)
-        self.pushButton_2 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.verticalLayout_2.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
         self.pushButton_2.setIconSize(QtCore.QSize(16, 20))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.verticalLayout.addWidget(self.pushButton_2)
-        self.pushButton_3 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.verticalLayout_2.addWidget(self.pushButton_2)
+        self.pushButton_3 = QtWidgets.QPushButton(Form)
         self.pushButton_3.setIconSize(QtCore.QSize(16, 20))
         self.pushButton_3.setObjectName("pushButton_3")
-        self.verticalLayout.addWidget(self.pushButton_3)
-        self.pushButton_4 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.verticalLayout_2.addWidget(self.pushButton_3)
+        self.pushButton_4 = QtWidgets.QPushButton(Form)
         self.pushButton_4.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
         self.pushButton_4.setIconSize(QtCore.QSize(16, 20))
         self.pushButton_4.setObjectName("pushButton_4")
-        self.verticalLayout.addWidget(self.pushButton_4)
+        self.verticalLayout_2.addWidget(self.pushButton_4)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_2.addItem(spacerItem)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pushButton_5 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_5 = QtWidgets.QPushButton(Form)
         self.pushButton_5.setObjectName("pushButton_5")
         self.horizontalLayout.addWidget(self.pushButton_5)
-        self.pushButton_6 = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton_6 = QtWidgets.QPushButton(Form)
         self.pushButton_6.setObjectName("pushButton_6")
         self.horizontalLayout.addWidget(self.pushButton_6)
-        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.verticalLayout.addLayout(self.verticalLayout_2)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "OstiClass"))
+        Form.setWindowTitle(_translate("Form", "Form"))
         self.pushButton.setText(_translate("Form", "Optical data"))
         self.pushButton_2.setText(_translate("Form", "Bone data"))
         self.pushButton_3.setText(_translate("Form", "Other features"))
