1810f80188da0f62c6248850ff6c7d36250dec8e
Kaname21Miura <miukana21@gmail.com>
2019-06-06 16:02:44 +0900

    mainUiを作りました
    OstiClass.pyを動かすとUiが表示されます。まだスロットは定義していません。

--- OstiClass.py
+++ OstiClass.py
@@ -7,13 +7,14 @@ Created on Tue May 28 10:55:53 2019
 """
 import sys
 from PyQt5 import QtWidgets
-from osticlassMainUi import Ui_Form
+from mainUi import Ui_OptiClassMain
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui = Ui_Form()
+        self.ui = Ui_OptiClassMain()
         self.ui.setupUi(self)
+
         
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
